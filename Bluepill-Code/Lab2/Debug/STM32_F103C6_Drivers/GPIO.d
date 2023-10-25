05aa3bd7e9e37c93edfc3801f6d6d4faced3018a
alaa wahba <alaawahbaa13@gmail.com>
2023-10-25 21:48:18 +0300

    STM32 code

---
+++ Bluepill-Code/Lab2/Debug/STM32_F103C6_Drivers/GPIO.d
@@ -0,0 +1,13 @@
+STM32_F103C6_Drivers/GPIO.o: ../STM32_F103C6_Drivers/GPIO.c \
+ ../STM32_F103C6_Drivers/inc/GPIO.h \
+ ../STM32_F103C6_Drivers/inc/STM32_F103x6.h \
+ ../STM32_F103C6_Drivers/inc/Platform_types.h \
+ ../STM32_F103C6_Drivers/inc/RCC.h
+
+../STM32_F103C6_Drivers/inc/GPIO.h:
+
+../STM32_F103C6_Drivers/inc/STM32_F103x6.h:
+
+../STM32_F103C6_Drivers/inc/Platform_types.h:
+
+../STM32_F103C6_Drivers/inc/RCC.h:
---
+++ Bluepill-Code/Lab2/Debug/STM32_F103C6_Drivers/USART.d
@@ -0,0 +1,17 @@
+STM32_F103C6_Drivers/USART.o: ../STM32_F103C6_Drivers/USART.c \
+ ../STM32_F103C6_Drivers/inc/USART.h ../STM32_F103C6_Drivers/inc/GPIO.h \
+ ../STM32_F103C6_Drivers/inc/STM32_F103x6.h \
+ ../STM32_F103C6_Drivers/inc/Platform_types.h \
+ ../STM32_F103C6_Drivers/inc/RCC.h ../STM32_F103C6_Drivers/inc/NVIC.h
+
+../STM32_F103C6_Drivers/inc/USART.h:
+
+../STM32_F103C6_Drivers/inc/GPIO.h:
+
+../STM32_F103C6_Drivers/inc/STM32_F103x6.h:
+
+../STM32_F103C6_Drivers/inc/Platform_types.h:
+
+../STM32_F103C6_Drivers/inc/RCC.h:
+
+../STM32_F103C6_Drivers/inc/NVIC.h:
